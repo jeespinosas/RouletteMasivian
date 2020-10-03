212d2d0278df50dbeb1aae82d5a821db08d4caee
Jorge Espinosa <jespinosa@sumset.com>
2020-10-03 09:10:56 -0500

    Create a new project

---
+++ roulette/src/test/java/co/back/test/masivian/roulette/RouletteApplicationTests.java
@@ -0,0 +1,13 @@
+package co.back.test.masivian.roulette;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RouletteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
